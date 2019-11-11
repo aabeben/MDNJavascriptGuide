cf63699745ebec70237c8f1b7ee7a829900b8ebf
Beny Hasanudin <bhasanudin@gmail.com>
2019-11-11 08:55:11 +0700

    let const hoisted but not initialized

--- src/basics.js
+++ src/basics.js
@@ -1,27 +1,2 @@
-/**
- * Example 1
- */
-/*
-console.log(x === undefined);
-var x = 3;
-*/
-var x;
-console.log(x === undefined);
-x = 3
-/**
- * Example 2
- */
-// will return a value of undefined
-/*
-var myvar = 'my value';
-(function(){
-    console.log(myvar); // undefined
-    var myvar = 'local variable';
-})();
- */
-var myvar = 'my value';
-(function(){
-    var myvar;
-    console.log(myvar);
-    myvar = 'local variable';
-})();
+console.log(x);
+let x = 3;
