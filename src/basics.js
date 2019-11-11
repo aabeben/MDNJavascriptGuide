04f04cae612b941f9749e31747e3f41ca0e3c2e8
Beny Hasanudin <bhasanudin@gmail.com>
2019-11-11 09:09:51 +0700

    another operator not get converted into string

--- src/basics.js
+++ src/basics.js
@@ -1,8 +1,2 @@
-var answer = 42;
-console.log(answer);
-answer = 'Thanks for all the fish...';
-console.log(answer);
-var x = 'The answer is '+42;
-console.log(x);
-var y = 42 + ' is the answer';
-console.log(y);
+console.log('37' - 7);
+console.log('37' + 7);
