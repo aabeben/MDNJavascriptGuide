d64f4673058a6609824193cac39059a51635ea65
Beny Hasanudin <bhasanudin@gmail.com>
2019-11-11 08:36:09 +0700

    undefined become false in boolean context, and NaN in numeric context.Null become 0 in numeric context

--- src/basics.js
+++ src/basics.js
@@ -1,9 +1,10 @@
+var input= true;
+if(input === undefined) {
+    console.log('the value of input was: '+input);
+} else {
+    console.log('the value of input was: '+input);
+}
 var a;
-console.log('The value of a is ' + a);
-console.log('The value of b is '+b);
-var b;
-console.log('The value of c is '+c);
-let x;
-console.log('The value of x is '+x);
-console.log('The value of y is '+y);
-let y;
+a + 2;
+var n = null;
+console.log(n*32, n?' false':' true');
