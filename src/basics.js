3ac642d44ee6553ea1affba9d9f969937f6c4cd8
Beny Hasanudin <bhasanudin@gmail.com>
2020-01-09 02:45:13 +0700

    Literal RegExp

--- src/basics.js
+++ src/basics.js
@@ -1,23 +1,9 @@
-/*  Literal Objek yang ditingkatkan
+/*  Literal ReExp
  *
- *    Pada ES2015, literal objek diperluaskan mendukung men-set purnarupa saat pembangunan,
- *  steno untuk penugasan foo: foo, pendefinisian metode, pemanggilan super, dan perhitungan
- *  nama properti dengan ekspresi.
- *
- *    Digabungkan, ini semua juga lebih mendekatkan keduanya yakni literal objek dan deklarasi class
- *  dan mengizinkan rancangan berbasis-objek memanfaatkan beberapa kenyamanan yang sama.
+ *    Sebuah literal regex (yang akan didefinisikan terperinci belakangan)
+ *  adalah sebuah pola yang dipagar di antara dua garis miring.
  */
 
-var obj = {
-  // __proto__
-  __proto__: theProtoObj,
-  // Steno untuk 'handler: handler'
-  handler,
-  // Metode
-  toString(){
-	  // Memanggil super
-	  return 'd '+super.toString();
-  },
-  // Nama properti hasil hitungan (dinamis)
-  ['prop_'+(()=>42)()]:42
-}
+// Berikut adalah sebuah contoh dari sebuah literal regex.
+
+var re = /ab+c/;
