0118a1f159c1da47bfbaa399875620b4b150572c
Beny Hasanudin <bhasanudin@gmail.com>
2020-01-09 00:53:28 +0700

    Koma selain yg mengekor saja yg dianggap ada
    Pada contoh di berikut, panjangnya larik ialah empat dan
    myList[0] dan myList[2] adalah hilang

--- src/basics.js
+++ src/basics.js
@@ -1,8 +1,3 @@
-// Jika Anda menyertakan sebuah koma yang mengekor pada akhir dari sebuah daftar elemen-elemen
-// Koma tersebut akan diabaikan.
-// Pada contoh berikut, panjangnya larik adalah tiga.
-// Tidak ada myList[3].
-// Semua koma lainnya di dalam daftar mengindikasikan sebuah elemen baru.
-// Catatan koma-koma yang mengekor akan menimbulkan galat pada versi tua peramban
-// dan merupakan praktek terbaik untuk membuang koma-koma tersebut.
-let myList = ['home', , 'school',];
+// Pada contoh berikut, panjangnya larik ialah empat, dan
+// myList[0] dan myList[2] adalah hilang
+let myList = [;,'home',,'school']
