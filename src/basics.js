790da7422be60794bf05fd2ee71b8e2237628bdf
Beny Hasanudin <bhasanudin@gmail.com>
2020-01-09 01:07:40 +0700

    Membetulkan yang salah akibat konflik

--- src/basics.js
+++ src/basics.js
@@ -1,7 +1,3 @@
 // Pada contoh berikut, panjangnya larik ialah empat, dan
 // myList[0] dan myList[2] adalah hilang
-<<<<<<< HEAD
 let myList = [,'home',,'school']
-=======
-let myList = [;,'home',,'school']
->>>>>>> 0118a1f159c1da47bfbaa399875620b4b150572c
