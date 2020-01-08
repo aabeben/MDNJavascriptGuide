81b778558a879ff7e1b4279e6d09a72df9ff1f59
Beny Hasanudin <bhasanudin@gmail.com>
2020-01-09 03:26:49 +0700

    Pertukaran untaian

--- src/basics.js
+++ src/basics.js
@@ -1,11 +1,33 @@
-/*  Ada dapat memanggil sembarang metode objek untaian pada nilai literal untaian.
+/*    Pada ES2015, literal cetakan juga tersedia. Literal cetakan dipagari oleh
+ *  karakter back-tick (`)(aksen grave) alih-alih tanda kutip ganda atau tunggal.
  *
- *  JavaScript mengkonversikan literal untaian menjadi sebuah objek untaian sementara secara
- *  otomatis, panggil metode, kemudian campakkan objek untaian sementara.
+ *    Untaian cetakan menyediakan gula sintaktik membangun untaian. (Ini mirip dengan
+ *  fitur pertukaran untaian pada Perl, Python, dan selebihnya.)
  *
- *  Anda juga dapat menggunakan properti String.length pada literal untaian.
+ *    Pilihan, sebuah tanda dapat ditambahkan memperkenankan pembangunan untaian diubahsesuaikan,
+ *  menghindari serangan injeksi, atau membangun susunan data tingkat di atasnya dari isi untaian.
  */
 
-// Akan mencetak jumlah simbol pada untaian mencakup spasiputih.
+// Pembuatan literal untaian dasar
+`In JavaScript '\n' is a line-feed.`
 
-console.log("John's cat".length); // Dalam hal ini, 10.
+// untaian banyak baris
+` Pada JavaScript, untaian cetakan dapat lari
+  pada lebih banyak baris, yang tidak dapat dilakukan
+  pada tanda kutip ganda atau tunggal.
+`
+
+// Penukaran untaian
+var name = 'Bob', time = 'today';
+`Hello ${name}, how are you ${time}`;
+
+// Membangun sebuah awalan permintaan HTTP digunakan untuk menterjemahkan
+// penggantian dan pembangunan
+
+POST`http://foo.org/bar?a=${a}&b=${b}
+     Content-Type: application/json
+     X-Credentials: ${credentials}
+     {
+         "foo":${foo},
+	 "bar":${bar}
+     }`(myOnReadyStateChangeHandler);
