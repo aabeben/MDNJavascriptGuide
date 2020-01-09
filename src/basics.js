6a60854c022ea029afc0945197b278ee727efa66
Beny Hasanudin <bhasanudin@gmail.com>
2020-01-09 08:07:57 +0700

    Melarikan diri sebuah karakter slash belakang di dalam untaian

--- src/basics.js
+++ src/basics.js
@@ -1,13 +1,7 @@
-/*  Karakter pelarian diri
- *
- *    Untuk karakter-karakter yang tidak terdaftar pada tabel, awalan slash belakang
- *  diabaikan, tetapi pemakaian ini sudah usang dan harus dihindari.
- *
- *    Anda dapat menyisipkan sebuah tanda kutip di dalam sebuah untaian dengan mengawalinya
- *  dengan sebuah slash belakang.
- *
- *    Ini dikenal sebagai melarikan diri tanda kutip. Sebagai contoh:
+/* Untuk menyertakan sebuah literal slash belakang
+ * di dalam sebuah untaian, Anda wajib melarikan diri karakter slash belakang.
+ * Sebagai contoh, untuk menugaskan jalan setapak berkas c:\temp ke sebuah untaian,
+ * gunakan berikut:
  */
 
-var quote = "He read \"The Cremation of Sam McGee\" by R.W> Service.";
-console.log(quote); // Hasilnya ialah seperti ini He read "The Cremation of Sam McGee" by R.W. Service.
+var home = 'c:\\temp';
