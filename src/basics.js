e0cb3dbe652e559819d327e30f20fb5671d515d2
Beny Hasanudin <bhasanudin@gmail.com>
2020-01-09 07:28:31 +0700

    Karakter khusus

--- src/basics.js
+++ src/basics.js
@@ -1,33 +1,47 @@
-/*    Pada ES2015, literal cetakan juga tersedia. Literal cetakan dipagari oleh
- *  karakter back-tick (`)(aksen grave) alih-alih tanda kutip ganda atau tunggal.
+/*  Menggunakan karakter khusus di dalam untaian
  *
- *    Untaian cetakan menyediakan gula sintaktik membangun untaian. (Ini mirip dengan
- *  fitur pertukaran untaian pada Perl, Python, dan selebihnya.)
- *
- *    Pilihan, sebuah tanda dapat ditambahkan memperkenankan pembangunan untaian diubahsesuaikan,
- *  menghindari serangan injeksi, atau membangun susunan data tingkat di atasnya dari isi untaian.
+ *  Tambahan pada karakter lazim, Anda juga dapat menyertakan karakter khusus di dalam untaian,
+ *  seperti yang tampak pada contoh berikut.
  */
 
-// Pembuatan literal untaian dasar
-`In JavaScript '\n' is a line-feed.`
-
-// untaian banyak baris
-` Pada JavaScript, untaian cetakan dapat lari
-  pada lebih banyak baris, yang tidak dapat dilakukan
-  pada tanda kutip ganda atau tunggal.
-`
-
-// Penukaran untaian
-var name = 'Bob', time = 'today';
-`Hello ${name}, how are you ${time}`;
-
-// Membangun sebuah awalan permintaan HTTP digunakan untuk menterjemahkan
-// penggantian dan pembangunan
-
-POST`http://foo.org/bar?a=${a}&b=${b}
-     Content-Type: application/json
-     X-Credentials: ${credentials}
-     {
-         "foo":${foo},
-	 "bar":${bar}
-     }`(myOnReadyStateChangeHandler);
+/*
+ *   Tabel: karakter khusus JavaScript
+ *
+ *   Karakter	Arti
+ *   \0		Bait null
+ *   
+ *   \b		Spasi belakang
+ *
+ *   \f		Umpan Formulir
+ *
+ *   \n		Baris baru
+ *
+ *   \r		Gerbong balik
+ *
+ *   \t		Tab
+ *
+ *   \v		Tab vertikal
+ *
+ *   \'		Apostrof atau kutip tunggal
+ *
+ *   \"		Kutip ganda
+ *
+ *   \\		Karakter slash belakang
+ *
+ *   \XXX	Karakter dengan pengkodean Latin-1 ditentukan oleh sampai dengan 
+ *   		3 digit oktal XXX antara 0 dan 377.
+ *   		Sebagai contoh, \251 ialah urutan okta untuk simbol hak cipta
+ *
+ *   \xXX	Karakter dengan pengkodean Latin-1 ditentukan oleh dua digit heksadesimal
+ *   		antara 00 dan FF.
+ *
+ *   		Sebagai contoh, \xA9 ialah urutan heksadesimal untuk simbol hak cipta.
+ *   \uXXXX	Karakter Unikode ditentukan oleh empat digit heksadesimal XXXX.
+ *		Sebagai contoh, \u00A9 ialah urutan Unikode untuk simbol hak cipta.
+ *		Lihat urutan pelarian diri Unikode.
+ *
+ *   \u{XXXXX}	Pelarian titik kode Unikode.
+ *   		Sebagai contoh, \u{2F804} ialah sama dengan pelarian Unikode sederhana
+ *   		\uD87E\uDC04.
+ */
+'one line \n another line'
