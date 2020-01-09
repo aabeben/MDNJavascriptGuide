c154db1aa1c8cae52cd91e2b041491123cf6e7af
Beny Hasanudin <bhasanudin@gmail.com>
2020-01-09 08:18:43 +0700

    Heredoc pada JavaScript

--- src/basics.js
+++ src/basics.js
@@ -1,10 +1,10 @@
-/* Anda juga dapat melarikan diri ganti baris dengan mengawali mereka dengan slash belakang.
- * Slash belakang dan ganti baris keduanya akan dihilangkan dari nilai untaian.
+/*  Sekalipun JavaScript tidak memiliki sintaks "heredoc",
+ *  Anda dapat menghampirinya dengan menambahkan sebuah pelarian ganti baris
+ *  dan sebuah ganti baris yang dilarikan diri pada akhir setiap baris:
  */
 
-var str = 'Untaian ini \
-dipecah \
-melintasi banyak \
-baris.';
-
-console.log(str); // Untaian ini dipecah melintasi banyak baris.
+var puisi = 
+'Mawar ialah merah,\n\
+Violet ialah biru,\n\
+Gula ialah manis,\n\
+dan demikianlah foo.';
